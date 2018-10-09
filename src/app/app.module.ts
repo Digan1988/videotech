8635d9d4719d507fd68e7c3c27b74a5b2c687ce1
Evgeny Voronov <eugene.voronov@girngm.ru>
2018-10-10 00:58:23 +0500

    заготовки основных компонентов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FeedbackformComponent } from './feedbackform/feedbackform.component';
-import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatInputModule} from '@angular/material';
+import {
+  MatButtonModule, 
+  MatCheckboxModule,
+  MatFormFieldModule, 
+  MatOptionModule, 
+  MatSelectModule, 
+  MatInputModule,
+  MatProgressBarModule
+} from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedbackformComponent
+    FeedbackformComponent,
+    HeaderComponent,
+    FooterComponent,
+    SolutionsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +36,9 @@ import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatOptionModule,
     MatFormFieldModule,
     MatOptionModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressBarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feedbackform/feedbackform.component.ts
+++ src/app/feedbackform/feedbackform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feedbackform',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedbackform.component.css']
 })
 export class FeedbackformComponent implements OnInit {
-
+  feedbackForm = new FormGroup({
+    name: new FormControl('', []),
+    phone: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email])
+  });
+  hasProgress: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.feedbackForm.value);
+
+    if(this.feedbackForm.valid){
+      this.hasProgress = true;
+      setTimeout(() =>{
+        this.hasProgress = false;
+      }, 2000);
+    }
   }
 
 }
